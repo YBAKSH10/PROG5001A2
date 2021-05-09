5b1e8ff4738c992e0676bff278f84119983dd270
YBAKSH10 <y.bakshi.10@student.scu.edu.au>
2021-05-09 16:59:52 -0700

    Yb_Prey
    Prey is being displayed.

--- Yb_Prey.java
+++ Yb_Prey.java
@@ -1,15 +1,50 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;  
 /**
  * Class Prey represents any prey object.
  *
  * @Yamini(23706019) 
  * @Version 2.0
  */
-abstract class Yb_Prey {
-    /**
-     * Method Prey
-     * Will help to change its position
-     */
-    public abstract void Position();
-    {
-    }    
+
+public class Yb_Prey extends JPanel {
+    Image img;
+    public Yb_Prey(String image_path, int width, int height) {
+        img = new ImageIcon(image_path).getImage();
+        //img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+        Dimension size = new Dimension(width, height);
+        setPreferredSize(size);
+    }
+    
+    public void paintComponent(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+        repaint();
+    }
+  
+  public static void main(String args[]) {
+    JFrame frame = new JFrame("Image prey");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Yb_Prey panel = new Yb_Prey("images/smile.png",1237, 50);
+    
+    frame.add(panel);
+    frame.setSize(300, 200);
+    frame.setVisible(true);
+  }
+  /**
+   * Method Position
+   * This allows prey to chnage its position
+   */
+  public void Position()
+  {
+      
+    }
 }
+
+
+  
