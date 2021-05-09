f8194e2c2e800be11eae89e1d0d38a726cbdd7cc
YBAKSH10 <82876901+YBAKSH10@users.noreply.github.com>
2021-05-09 21:25:18 +0530

    Yb_Snake
    This class displays the snake by using a for loop.

--- Yb_Snake.java
+++ Yb_Snake.java
@@ -4,24 +4,82 @@
  * @author Yamini
  * @version 2.0 (23706019)
  */
-abstract class Yb_Snake
-{
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+public class Yb_Snake extends JPanel {
+    Image face;
+    Image tail;
+    public Yb_Snake() {
+        face= new ImageIcon("Images/face.png").getImage();
+        tail= new ImageIcon("Images/tail.png").getImage();
+        Dimension dimension = new Dimension(50,50);
+        setPreferredSize(dimension);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(face, 20, 20, null);
+        repaint();
+
+        int x_axis = 100;
+        int y_axis = 150;
+
+        for(int i=0; i<3; i++)
+        {
+            g.drawImage(tail, 90,100+(i*250), null);
+            //repaint();
+            //y_axis = y_axis + 30;
+        }
+
+        for(int j=0; j<4; j++)
+        { 
+            g.drawImage(tail,900,100+(j*250), null);
+            //repaint();
+            //x_axis = x_axis + 30;
+        }
+        repaint();
+        g.drawImage(tail, 100, 100, null);
+    }
+
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Image Of Snake");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Yb_Snake panel = new Yb_Snake();
+
+        frame.add(panel);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+
+
+    
     /** Method Move
      * This method will help the snake to move in various directions using the arrow keys.
      * 
      */
-    public abstract void Move();
+    public void Move()
+    {
+    }
     
     /** Method EatPrey
      * This method will allow snake to eat the prey
      * 
      */
-    public abstract void EatPrey();
+    public void EatPrey()
+    {
+    }
     
     /** Method Grow
      * This method will allow snake to eat the prey and grow.
      * 
      */
-    public abstract void Grow();
+    public void Grow()
+    {
+    }
 }
 
