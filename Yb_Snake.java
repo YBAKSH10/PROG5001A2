d073e1066d6aaa08c37cfddb2f47dbf8729d971d
YBAKSH10 <82876901+YBAKSH10@users.noreply.github.com>
2021-05-13 19:06:19 +0530

    Yb_Snake
    Applied for loop for the proper dimensions of snake

--- Yb_Snake.java
+++ Yb_Snake.java
@@ -1,9 +1,3 @@
-/**
- * The given snake class is an abstract class that defines its methods.
- *
- * @author Yamini
- * @version 2.0 (23706019)
- */
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -12,74 +6,88 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-public class Yb_Snake extends JPanel {
+
+/**
+ * Write a description of class Snake here.
+ *
+ * @author (Yamini)
+ * @version (2.0 or  May 13, 2021)
+ */
+public class Yb_Snake  extends JPanel
+{
+
     Image face;
     Image tail;
+
+    /**
+     * Constructor for objects of class Snake
+     */
     public Yb_Snake() {
-        face= new ImageIcon("Images/face.png").getImage();
-        tail= new ImageIcon("Images/tail.png").getImage();
-        Dimension dimension = new Dimension(50,50);
-        setPreferredSize(dimension);
+        // initialise instance variables   
+        face = new ImageIcon("images/face.png").getImage();
+        tail = new ImageIcon("images/tail.png").getImage();
+        //img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+        Dimension size = new Dimension(100, 100);
+        setPreferredSize(size);
+
     }
 
     public void paintComponent(Graphics g) {
-        g.drawImage(face, 20, 20, null);
+        g.drawImage(face, 100, 100, null);
         repaint();
 
-        int x_axis = 100;
-        int y_axis = 150;
-
-        for(int i=0; i<3; i++)
+        for(int i=1; i<=2;i++)
         {
-            g.drawImage(tail, 90,100+(i*250), null);
-            //repaint();
-            //y_axis = y_axis + 30;
+            g.drawImage(tail, 95+(i*42), 190, null);
+            repaint();
         }
 
-        for(int j=0; j<4; j++)
-        { 
-            g.drawImage(tail,900,100+(j*250), null);
-            //repaint();
-            //x_axis = x_axis + 30;
+        for(int j=0; j<=2;j++)
+        {
+            g.drawImage(tail, 100, 140+(j*25), null);
+            repaint();
         }
-        repaint();
-        g.drawImage(tail, 100, 100, null);
+
+        g.drawImage(tail, 180, 167, null);
+
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {        
         JFrame frame = new JFrame("Image Of Snake");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Yb_Snake panel = new Yb_Snake();
 
         frame.add(panel);
-        frame.setSize(300, 200);
+        frame.setSize(800, 600);
         frame.setVisible(true);
     }
 
-
-    
-    /** Method Move
-     * This method will help the snake to move in various directions using the arrow keys.
-     * 
+    /**
+     * Method Move
+     * this method will help the snake to move in various directions with the help of Arrow keys
      */
     public void Move()
     {
+
     }
-    
-    /** Method EatPrey
-     * This method will allow snake to eat the prey
-     * 
+
+    /**
+     * Method EatPrey
+     * the snake will change it direction through arrow keys
      */
     public void EatPrey()
     {
+
     }
-    
-    /** Method Grow
-     * This method will allow snake to eat the prey and grow.
-     * 
+
+    /**
+     * Method grow
+     * the snake will grow longer when it will eat prey
      */
     public void Grow()
     {
-    }
-}
 
+    }
+
+}
