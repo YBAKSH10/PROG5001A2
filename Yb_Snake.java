35d5fdf5ac846b35667a9170099f61f8e4a6dfe1
YBAKSH10 <82876901+YBAKSH10@users.noreply.github.com>
2021-05-23 00:15:47 -0700

    YB_Snake
    Made the exact dimensions and given the path so that snake could be in the panel itself

--- Yb_Snake.java
+++ Yb_Snake.java
@@ -19,75 +19,17 @@ public class YB_Snake  extends JPanel
     Image face;
     Image tail;
 
+
     /**
      * Constructor for objects of class Snake
      */
-    public YB_Snake() {
+    public YB_Snake(){
         // initialise instance variables   
-        face = new ImageIcon("images/face.png").getImage();
-        tail = new ImageIcon("images/tail.png").getImage();
+        face = new ImageIcon("Images/face.png").getImage();
+        tail = new ImageIcon("Images/tail.png").getImage();
         //img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
         Dimension size = new Dimension(100, 100);
         setPreferredSize(size);
 
     }
-
-    public void paintComponent(Graphics g) {
-        g.drawImage(face, 100, 100, null);
-        repaint();
-
-        for(int i=1; i<=2;i++)
-        {
-            g.drawImage(tail, 95+(i*42), 190, null);
-            repaint();
-        }
-
-        for(int j=0; j<=2;j++)
-        {
-            g.drawImage(tail, 100, 140+(j*25), null);
-            repaint();
-        }
-
-        g.drawImage(tail, 180, 167, null);
-
-    }
-
-    public static void main(String[] args) {        
-        JFrame frame = new JFrame("Image Of Snake");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        YB_Snake panel = new YB_Snake();
-
-        frame.add(panel);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-    }
-
-    /**
-     * Method Move
-     * this method will help the snake to move in various directions with the help of Arrow keys
-     */
-    public void Move()
-    {
-
-    }
-
-    /**
-     * Method EatPrey
-     * the snake will change it direction through arrow keys
-     */
-    public void EatPrey()
-    {
-
-    }
-
-    /**
-     * Method grow
-     * the snake will grow longer when it will eat prey
-     */
-    public void Grow()
-    {
-
-    }
-
 }
