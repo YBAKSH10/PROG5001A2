e9090b79fe2dcc00af05a817e86646cc7c9d43b3
YBAKSH10 <82876901+YBAKSH10@users.noreply.github.com>
2021-05-23 00:11:51 -0700

    YB_Prey
    This is the prey class that has the details of the prey. It includes the dimensions of the prey.

--- Yb_Prey.java
+++ Yb_Prey.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Image;
 import javax.swing.JFrame;
+import java.awt.EventQueue;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;  
@@ -13,38 +14,47 @@ import javax.swing.JLabel;
  * @Version 2.0
  */
 
-public class YB_Prey extends JPanel {
-    Image img;
-    public YB_Prey(String image_path, int width, int height) {
-        img = new ImageIcon(image_path).getImage();
-        //img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
-        Dimension size = new Dimension(width, height);
+public class YB_Prey extends JFrame {
+    // Image object taken as "smile"
+    public Image smile;
+    public int PreyAlongX;
+    public int PreyAlongY;
+    
+    
+    public YB_Prey() {
+        smile = new ImageIcon("Images/prey.png").getImage();
+        Dimension size = new Dimension(10, 10);
         setPreferredSize(size);
     }
-    
-    public void paintComponent(Graphics g) {
-        g.drawImage(img, 0, 0, null);
-        repaint();
-    }
-  
-  public static void main(String args[]) {
-    JFrame frame = new JFrame("Image prey");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    YB_Prey panel = new YB_Prey("images/smile.png",1237, 50);
-    
-    frame.add(panel);
-    frame.setSize(300, 200);
-    frame.setVisible(true);
-  }
-  /**
-   * Method Position
-   * This allows prey to chnage its position
-   */
-  public void Position()
-  {
       
+      
+    /**
+    * Method Position
+    * This allows prey to chnage its position
+    */
+    public void position(int RAND_POS,int DOT_SIZE)
+    {
+      int r = (int) (Math.random() * RAND_POS);
+        PreyAlongX= ((r * DOT_SIZE));
+    
+        r = (int) (Math.random() * RAND_POS);
+        PreyAlongY = ((r * DOT_SIZE));
     }
+        
+    public void checkPrey(int x, int y , int RAND_POS, int DOT_SIZE) 
+    {
+    
+        if ((x == PreyAlongX) && (y == PreyAlongY)) {
+
+            YB_GameBoard.dots++;
+            position(RAND_POS,DOT_SIZE);
+        }
+    }     
 }
+    
+ 
+
+
 
 
   
