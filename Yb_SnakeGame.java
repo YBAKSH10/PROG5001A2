9d5eb7476ed8ab9f7b214d4cb9f2b6a4dcf16d04
YBAKSH10 <82876901+YBAKSH10@users.noreply.github.com>
2021-05-30 14:50:51 +0530

    YB_SnakeGame
    In this class a panel is set.
    Right panel is initialised in the starting of the code.
    Objects is made so that the quit button works suing getSource

--- Yb_SnakeGame.java
+++ Yb_SnakeGame.java
@@ -15,7 +15,7 @@ import javax.swing.LayoutStyle;
 import javax.swing.border.LineBorder;
 import java.awt.event.*;
 import javax.swing.JOptionPane;
-
+import javax.swing.*;
 /**
  * This program demonstrates how to use JFrame and LayoutManager.
  * @author Yamini
@@ -32,7 +32,8 @@ public class YB_SnakeGame extends JFrame implements ActionListener {
     private JButton Quit;
     private Graphics g;
     private JLabel MySnakeGame; 
-
+    JPanel rightPanel = new JPanel(new GridBagLayout());
+    YB_GameBoard leftPanel = new  YB_GameBoard(this);
     public YB_SnakeGame(String SnakeGame) {  
         super(SnakeGame);
         //setTitle(SnakeGameTille);
@@ -61,7 +62,7 @@ public class YB_SnakeGame extends JFrame implements ActionListener {
         //TOP PANEL
         JPanel topPanel = new JPanel(new GridBagLayout());
         topPanel.setBackground(Color.BLUE);
-        topPanel.setBounds(10,10,1350,50);
+        topPanel.setSize(1350,50);
         MySnakeGame.setForeground(Color.white);
         GridBagConstraints topconstraint= new GridBagConstraints();
         topconstraint.anchor = GridBagConstraints.NORTH;
@@ -69,27 +70,30 @@ public class YB_SnakeGame extends JFrame implements ActionListener {
         topPanel.add(MySnakeGame,topconstraint);
 
         //  LEFT PANEL 
-        JPanel leftPanel = new JPanel(new GridBagLayout());
+
         leftPanel.setBackground(Color.black);
-        leftPanel.setBounds(10,10,550,700);        
+        leftPanel.setBounds(20,60,550,700);        
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.WEST;
         constraints.insets = new Insets(50, 50, 50, 50);
 
         // GAME OVER
         constraints.gridx = 0;
-        constraints.gridy = 0;     
+        constraints.gridy = 1;     
+
+        ClickToPlay.addActionListener(this);
+        leftPanel.add(ClickToPlay, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 2;
         GameOver.setForeground(Color.white);
         leftPanel.add(GameOver,constraints);
 
+        GameOver.setVisible(false);
+        ClickToPlay.setVisible(false);
         // CLICK TO PLAY
-        constraints.gridx = 0;
-        constraints.gridy = 1; 
-        ClickToPlay.addActionListener(this);
-        leftPanel.add(ClickToPlay, constraints);
 
         // RIGHT PANEL
-        JPanel rightPanel = new JPanel(new GridBagLayout());
+        //JPanel rightPanel = new JPanel(new GridBagLayout());
         GridBagConstraints constraint = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.EAST;
         constraints.insets = new Insets(10, 10, 10, 10);
@@ -98,19 +102,19 @@ public class YB_SnakeGame extends JFrame implements ActionListener {
         constraints.gridx = 0;
         constraints.gridy = 0;     
         rightPanel.add(TopLayersScore, constraints);
-        TopLayersScore.setBorder(new LineBorder(Color.blue,2));
+        TopLayersScore.setBorder(new LineBorder(Color.BLACK,2));
 
         //CURRENT PLAYER SCORE
         constraints.gridx = 0;  
         constraints.gridy = 1;
         rightPanel.add(CurrentPlayerScore, constraints);
-        CurrentPlayerScore.setBorder(new LineBorder(Color.blue,2));
+        CurrentPlayerScore.setBorder(new LineBorder(Color.BLACK,2));
 
         //NAME PANEL
         constraints.gridx = 0;  
         constraints.gridy = 2;
         rightPanel.add(Name, constraints);
-        Name.setBorder(new LineBorder(Color.blue,2));
+        Name.setBorder(new LineBorder(Color.BLACK,2));
 
         //SNAKE IMAGE
         constraints.gridx = 0;  
@@ -123,40 +127,56 @@ public class YB_SnakeGame extends JFrame implements ActionListener {
         constraints.gridx = 0;  
         constraints.gridy = 4;
         rightPanel.add(Quit, constraints);     
+        Quit.addActionListener(this);
 
          
         add(topPanel);
         add(leftPanel);
         add(rightPanel);
+        // To make the action Listener works
+        rightPanel.setVisible(false);
         setLocationRelativeTo(null);
         setVisible(true);
         pack();
     }
 
+    // Displays the screen once the game gets over
+    public void GameOverScreen()
+    {
+        rightPanel.setVisible(true);
+        GameOver.setVisible(true);
+        ClickToPlay.setVisible(true);
+        leftPanel.setLayout(null);
+        GameOver.setBounds(200,200,100,20);
+        ClickToPlay.setBounds(200,340,120,30);
+
+    }
+
     public void actionPerformed(ActionEvent e) {
-        //this.setVisible(false);
 
-        YB_GameBoard game = new YB_GameBoard();
-        game.setVisible(true);
-        JFrame frame1 = new JFrame();
-        frame1.add(new YB_GameBoard());
-
-        frame1.setResizable(false);
-        frame1.pack();
-
-        frame1.setTitle("FRAME-1 SNAKE");
-        frame1.setLocationRelativeTo(null);
-        frame1.setVisible(true);
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Calling class YB_SnakeGame through the object on click play event and quit
+      
+        YB_SnakeGame Game1 = null;
+        // [https://www.codota.com/code/java/methods/java.awt.event.ActionEvent/getSource]
+        if(e.getSource()==ClickToPlay)
+        {
+            Game1= new YB_SnakeGame("snake");
+            Game1.setSize(1200,1200);
+            // [https://www.codota.com/code/java/methods/javax.swing.JFrame/dispose]
+            this.dispose();
+        }
+        else if(e.getSource()==Quit)        
+        //[https://www.edureka.co/blog/system-exit-in-java/]
+        System.exit(0);
     }
 
     //make the form apprear in the screen centre
     //public static void main(String[] args) {        
     //EventQueue.invokeLater(() -> {
     //create the game with a game title as follow: The Snake Game (C) Your_Name
-    // JFrame sgame = new YB_SnakeGame("Snake");
+    //JFrame sgame = new YB_SnakeGame("Snake");
     // sgame.setVisible(true);
-    //            sgame.setSize(500,800);
+    //sgame.setSize(1300,800);
     // });
     // }
 
